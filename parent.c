4cdea69c332dfdfcd010761643b5af95fdf40016
kclark85 <32521814+kclark85@users.noreply.github.com>
2019-07-01 14:01:48 -0600

    Update parent.c

--- parent.c
+++ parent.c
@@ -7,18 +7,16 @@
 #include <assert.h>
 #include <sys/unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 #include "syscall.h"
-#include "eye2eh.c"
+
+int eye2eh(int i, char *buffer, int buffersize, int base);
 
 void handler(int signum){
     if (signum == SIGCHLD) {
-        WRITESTRING("Exit status: ")
-        WRITEINT(signum, sizeof(signum))
+        WRITESTRING("Child status: ")
+        WRITEINT(signum, 5)
 
-    }
-    else {
-        WRITESTRING("Unexpected signal: ")
-        WRITEINT(signum,sizeof(signum))
     }
     exit(0);
 }
@@ -30,7 +28,6 @@ int main(int argc, char *argv[]) {
     action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
 
     assert (sigaction (SIGCHLD, &action, NULL) == 0);
-    assert (sigaction (SIGINT, &action, NULL)== 0);
 
     pid_t ChildZero = fork();
     int result;
@@ -41,7 +38,7 @@ int main(int argc, char *argv[]) {
     else if (ChildZero == 0) {
         //child
         //printf("Child Process: %d \n Parent Process: %d \n", ChildZero, getppid());
-        execl("./child", "child", (char *)0);
+        execl("./child", "child", (char *)NULL);
         perror("execl");
         return -1;
     }
@@ -50,12 +47,10 @@ int main(int argc, char *argv[]) {
         int cpid = ChildZero;
         for(int i = 0; i < 5; i++) {
             WRITESTRING("\nParent stopping child\n")
-            assert (kill(cpid, SIGSTOP) == 0);
-            //assert (kill(getpid(), SIGCONT) == 0);
+            assert (kill(cpid, SIGSTOP) == 0); //stop child
             assert(sleep(2)==0);
             WRITESTRING("\nParent starting child\n")
-            assert (kill(cpid, SIGCONT) == 0);
-            //assert (kill(getpid(), SIGSTOP) == 0);
+            assert (kill(cpid, SIGCONT) == 0); //start child
             assert(sleep(2)==0);
 
         }
@@ -63,6 +58,6 @@ int main(int argc, char *argv[]) {
         assert(kill(cpid, SIGINT) == 0);
         assert(waitpid(-1, &result, 0) != -1);
 //        assert(printf("Process %d exited with status : %d", ChildZero, WEXITSTATUS(result)) !=0);
+        pause();
     }
-    pause();
 }
